a308422897e55e5c7e83134b7e5a7d9ff1d9cf10
peacemiraclepeter <mpeterpeace@byupathway.edu>
2026-06-09 10:18:26 +0100

    week 05 project

---
+++ week05/Mindfulness/Activity.cs
@@ -0,0 +1,81 @@
+using System;
+using System.Threading;
+
+public class Activity
+{
+    private string _name;
+    private string _description;
+    private int _duration;
+
+    public Activity(string name, string description)
+    {
+        _name = name;
+        _description = description;
+    }
+
+    public int GetDuration()
+    {
+        return _duration;
+    }
+
+    public void DisplayStartingMessage()
+    {
+        Console.Clear();
+        Console.WriteLine($"Welcome to the {_name} Activity.\n");
+        Console.WriteLine(_description);
+        Console.WriteLine();
+
+        Console.Write("How long, in seconds, would you like for your session? ");
+        _duration = int.Parse(Console.ReadLine());
+
+        Console.WriteLine();
+        Console.WriteLine("Get ready...");
+        ShowSpinner(3);
+    }
+
+    public void DisplayEndingMessage()
+    {
+        Console.WriteLine();
+        Console.WriteLine("Well done!!");
+        ShowSpinner(3);
+
+        Console.WriteLine();
+        Console.WriteLine($"You have completed another {_duration} seconds of the {_name} Activity.");
+        ShowSpinner(3);
+    }
+
+    protected void ShowSpinner(int seconds)
+    {
+        string[] spinner = { "|", "/", "-", "\\" };
+
+        DateTime endTime = DateTime.Now.AddSeconds(seconds);
+
+        int i = 0;
+
+        while (DateTime.Now < endTime)
+        {
+            Console.Write(spinner[i]);
+            Thread.Sleep(250);
+            Console.Write("\b \b");
+
+            i++;
+
+            if (i >= spinner.Length)
+            {
+                i = 0;
+            }
+        }
+    }
+
+    protected void ShowCountdown(int seconds)
+    {
+        for (int i = seconds; i > 0; i--)
+        {
+            Console.Write(i);
+            Thread.Sleep(1000);
+            Console.Write("\b \b");
+        }
+
+        Console.WriteLine();
+    }
+}
--- week05/Mindfulness/Program.cs
+++ week05/Mindfulness/Program.cs
@@ -1,9 +1,45 @@
 using System;
 
+// Creativity:
+// Added a Gratitude Activity.
+// Added activity statistics.
+// Prevented duplicate prompts until all prompts are used.
+
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Mindfulness Project.");
+        string choice = "";
+
+        while (choice != "4")
+        {
+            Console.Clear();
+
+            Console.WriteLine("Menu Options:");
+            Console.WriteLine("1. Start Breathing Activity");
+            Console.WriteLine("2. Start Reflection Activity");
+            Console.WriteLine("3. Start Listing Activity");
+            Console.WriteLine("4. Quit");
+            Console.WriteLine();
+
+            Console.Write("Select a choice from the menu: ");
+            choice = Console.ReadLine();
+
+            if (choice == "1")
+            {
+                BreathingActivity activity = new BreathingActivity();
+                activity.Run();
+            }
+            else if (choice == "2")
+            {
+                ReflectionActivity activity = new ReflectionActivity();
+                activity.Run();
+            }
+            else if (choice == "3")
+            {
+                ListingActivity activity = new ListingActivity();
+                activity.Run();
+            }
+        }
     }
 }
